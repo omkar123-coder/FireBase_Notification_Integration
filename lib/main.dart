4ad8f3dc503811cb1a622870ab7dd4c29f653662
Omkar Londhe <omkarlondhe.appdid@gmail.com>
2026-05-19 16:07:45 +0530

    Local Notification added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 import 'firebase_options.dart';
 
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(
+    RemoteMessage message) async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -11,23 +17,107 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.onBackgroundMessage(
+      _firebaseMessagingBackgroundHandler);
 
-  await FirebaseMessaging.instance.requestPermission(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
+  await NotificationRepository.notificationPlugin();
 
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class NotificationRepository {
+  static AndroidNotificationChannel channel =
+      const AndroidNotificationChannel(
+    'channel_id',
+    'channel_title',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.high,
+    playSound: true,
+  );
+
+  static Future<void> notificationPlugin() async {
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+   // Request permission (Android 13+)
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.requestNotificationsPermission();
+
+    // Android initialization
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    // iOS initialization
+    const DarwinInitializationSettings iosInitializationSettings =
+        DarwinInitializationSettings(
+        requestAlertPermission: true,
+        requestBadgePermission: true,
+        requestSoundPermission: true,
+      );
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: iosInitializationSettings,
+    );
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (details) async {
+        debugPrint("Notification clicked: ${details.payload}");
+      },
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+
+  Future<void> showNotification() async {
+    setState(() {
+      counter++;
+    });
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      "Testing $counter",
+      "How you doing ?",
+    NotificationDetails(
+      android: AndroidNotificationDetails(
+      NotificationRepository.channel.id,
+      NotificationRepository.channel.name,
+      channelDescription:
+        NotificationRepository.channel.description,
+        importance: Importance.high,
+        priority: Priority.high,
+        playSound: true,
+        // icon: '@mipmap/ic_launcher',
+        ),
+        iOS: const DarwinNotificationDetails(
+          presentAlert: true,
+          presentBadge: true,
+          presentSound: true,
+        ),
+      ),
+      // payload: 'Open from Local Notification',
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,8 +127,11 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Firebase Notification"),
         ),
-        body: const Center(
-          child: Text("FCM Working"),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: showNotification,
+            child: const Text("Show Notification"),
+          ),
         ),
       ),
     );
