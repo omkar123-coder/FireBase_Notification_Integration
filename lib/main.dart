d05178616a2d8810663e3b2b9c09660920f4a1ee
Omkar Londhe <omkarlondhe.appdid@gmail.com>
2026-05-21 12:43:31 +0530

    Messaging From Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,98 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'firebase_options.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  print("Background message received");
+  print("Title: ${message.notification?.title}");
+  print("Body: ${message.notification?.body}");
 }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  const AndroidInitializationSettings androidInitializationSettings =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
 
-  await FirebaseMessaging.instance.requestPermission(
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: androidInitializationSettings,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+  );
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  await messaging.requestPermission(
     alert: true,
     badge: true,
     sound: true,
   );
+
+  String? token = await messaging.getToken();
+  log("FCM TOKEN: $token");
+
+  FirebaseMessaging.onBackgroundMessage(
+    _firebaseMessagingBackgroundHandler,
+  );
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print("Message Recevied");
+
+    RemoteNotification? notification = message.notification;
+
+    if (notification != null) {
+      flutterLocalNotificationsPlugin.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+            'high_importance_channel',
+            'High Importance Notifications',
+            importance: Importance.max,
+            priority: Priority.high,
+          ),
+        ),
+      );
+    }
+  });
+
+  // // Notification Clicked
+  FirebaseMessaging.onMessageOpenedApp.listen(
+    (RemoteMessage message) {
+      print("Notification clicked");
+
+      print("Title: ${message.notification?.title}");
+      print("Body: ${message.notification?.body}");
+    },
+  );
+
+  RemoteMessage? initialMessage =
+      await FirebaseMessaging.instance.getInitialMessage();
+
+  if (initialMessage != null) {
+    print("App opened from terminated state");
+
+    print("Title: ${initialMessage.notification?.title}");
+    print("Body: ${initialMessage.notification?.body}");
+  }
 
   runApp(const MyApp());
 }
